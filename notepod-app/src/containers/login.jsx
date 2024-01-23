0e00ef84887012aa072b62ce61df09ab53d42dcd
Faraaz Mohsin <49964697+faraazmohsin@users.noreply.github.com>
2024-01-23 01:43:34 -0500

    auth

---
+++ notepod-app/src/containers/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=8f5da6f0e6644678a3cddab063ab3660&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+
+export default function Login() {
+    return (
+        <div>
+
+        </div>
+    )
+};
